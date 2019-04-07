f10a5f0ba5d2d88dfe876aa57a117e64d186760d
Andre Madarang <andre_madarang@hotmail.com>
2019-04-07 00:59:23 -0400

    Add 404 Page

---
+++ src/pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto py-16">
+      <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
+      <g-image src="../../static/404.svg" />
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Page Not Found'
+  }
+}
+</script>
+
