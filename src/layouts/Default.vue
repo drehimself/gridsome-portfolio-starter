d1d7116a2b151874d127364a7ef1399caed5080c
Andre Madarang <andre_madarang@hotmail.com>
2019-04-10 22:48:40 -0400

    Add responsive nav with hamburger menu

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="font-sans text-gray-800 leading-normal flex flex-col min-h-screen">
     <header class="border-t-14 border-green-700">
-      <nav class="container mx-auto flex flex-col sm:flex-row justify-between items-center py-8">
-        <div class="mb-8 sm:mb-0">
+      <nav class="container mx-auto flex flex-wrap justify-between items-center py-8">
+        <div>
           <g-link to="/"><g-image src="../../static/logo.svg" class="w-40" alt="logo" /></g-link>
         </div>
-        <ul class="uppercase tracking-wide font-bold flex items-center">
-          <li class="mr-8">
+        <div class="block sm:hidden">
+          <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
+            <svg class="current-color h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+          </button>
+        </div>
+        <ul
+          class="uppercase tracking-wide font-bold w-full block flex-grow sm:flex sm:flex-initial sm:w-auto items-center mt-8 sm:mt-0"
+          :class="isOpen ? 'block': 'hidden'"
+        >
+          <li class="mr-8 mb-6 sm:mb-0">
             <a v-if="$route.path === '/'" href="/#projects" v-scroll-to="'#projects'" class="text-gray-800 hover:text-gray-600">Projects</a>
             <g-link v-else to="/#projects" class="text-gray-800 hover:text-gray-600">Projects</g-link>
           </li>
-          <li class="mr-8">
+          <li class="mr-8 mb-6 sm:mb-0">
             <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" class="text-gray-800 hover:text-gray-600">About</a>
             <g-link v-else to="/#about" class="text-gray-800 hover:text-gray-600">About</g-link>
           </li>
-          <li class="mr-8">
+          <li class="mr-8 mb-6 sm:mb-0">
             <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="text-gray-800 hover:text-gray-600">Contact</a>
             <g-link v-else to="/#contact" class="text-gray-800 hover:text-gray-600">Contact</g-link>
           </li>
@@ -77,5 +85,20 @@ query {
   }
 }
 </static-query>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
 
 <style src="../main.css" />
