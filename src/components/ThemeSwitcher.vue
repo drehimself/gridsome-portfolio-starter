966b93ec27cb21a64b3d8cb992a67d48a2630840
Andre Madarang <andre_madarang@hotmail.com>
2019-05-29 12:38:24 -0400

    Move ThemeSwitcher to own component

---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,25 @@
+<template>
+  <a href="#" class="text-copy-primary hover:text-gray-600" @click.prevent="toggleTheme">
+    <svg v-if="theme === 'theme-light'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>
+    <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"/></svg>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    theme: {
+      type: String,
+      required: true,
+    }
+  },
+  methods: {
+    toggleTheme() {
+      const newTheme = this.theme === 'theme-light' ? 'theme-dark' : 'theme-light'
+      localStorage.setItem('theme', newTheme)
+      this.$emit('themeChanged', newTheme)
+    }
+  }
+}
+</script>
+
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -19,10 +19,7 @@
             <search-input />
           </li>
           <li class="mr-8 mb-6 lg:mb-0">
-            <a href="#" class="text-copy-primary hover:text-gray-600" @click.prevent="toggleTheme">
-              <svg v-if="theme === 'theme-light'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>
-              <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"/></svg>
-            </a>
+            <theme-switcher :theme="theme" @themeChanged="updateTheme" />
           </li>
           <li class="mr-8 mb-6 lg:mb-0">
             <a v-if="$route.path === '/'" href="/#projects" v-scroll-to="'#projects'" class="text-copy-primary hover:text-gray-600">Projects</a>
@@ -105,28 +102,28 @@ query {
 
 <script>
 import SearchInput from '../components/SearchInput'
+import ThemeSwitcher from '../components/ThemeSwitcher'
 
 export default {
   components: {
-    SearchInput
+    SearchInput,
+    ThemeSwitcher
   },
   mounted() {
     this.theme = localStorage.getItem('theme') || 'theme-light'
-
   },
   data() {
     return {
       isOpen: false,
-      theme: 'theme-light',
+      theme: '',
     }
   },
   methods: {
     toggle() {
       this.isOpen = !this.isOpen
     },
-    toggleTheme() {
-      this.theme = this.theme === 'theme-light' ? 'theme-dark' : 'theme-light'
-      localStorage.setItem('theme', this.theme)
+    updateTheme(theme) {
+      this.theme = theme
     }
   }
 }
