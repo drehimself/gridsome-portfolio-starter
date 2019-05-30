73191fae7b916bf0a8f675e2f52bc9014383d97e
Andre Madarang <andre_madarang@hotmail.com>
2019-05-30 03:09:11 -0400

    Make search keyboard accessible

---
+++ src/components/SearchFocus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  created() {
+    const component = this
+    this.handler = event => {
+      component.$emit('keyup', event)
+    }
+    window.addEventListener('keyup', this.handler)
+  },
+  beforeDestroy() {
+    window.removeEventListener('keyup', this.handler)
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    display: none;
+  }
+</style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="relative">
-    <input
-      type="text"
-      class="bg-background-form border border-gray-500 rounded-full px-4 pl-10 py-2 outline-none focus:border-green-500 w-80"
-      v-model="query"
-      @keyup="performSearch"
-      @keyup.esc="reset"
-    >
-    <div class="absolute top-0 ml-3" style="top:10px">
-      <svg fill="currentColor" class="text-gray-500 h-5 w-5" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>
-    </div>
-    <div
-      v-if="query.length > 0"
-      class="absolute top-0 right-0 text-2xl mr-3 cursor-pointer text-gray-600 hover:text-gray-800"
-      style="top:2px"
-      @click="reset"
-    >
-      &times;
+    <search-focus @keyup="focusSearch"></search-focus>
+
+    <div class="relative w-80">
+      <input
+        type="text"
+        placeholder="Search (Press  &quot;/&quot; to focus)"
+        class="bg-background-form border border-gray-500 rounded-full px-4 pl-10 py-2 outline-none focus:border-green-500 w-80"
+        v-model="query"
+        @keyup="performSearch"
+        @keyup.esc="reset"
+        @keydown.up.prevent="highlightPrev"
+        @keydown.down.prevent="highlightNext"
+        @keyup.enter="gotoLink"
+        ref="search"
+      >
+      <div class="absolute top-0 ml-3" style="top:10px">
+        <svg fill="currentColor" class="text-gray-500 h-5 w-5" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>
+      </div>
+      <div
+        v-if="query.length > 0"
+        class="absolute top-0 right-0 text-2xl mr-3 cursor-pointer text-gray-600 hover:text-gray-800"
+        style="top:2px;"
+        @click="reset"
+      >
+        &times;
+      </div>
     </div>
     <transition name="fade">
       <div v-if="query.length > 0" class="normal-case absolute border left-0 right-0 w-108 text-left mb-4 mt-2 rounded-lg shadow overflow-hidden z-10">
@@ -27,6 +36,7 @@
             :href="post.item.path"
             @click="reset"
             class="bg-background-form border-b border-gray-400 text-xl cursor-pointer p-4 search-hover"
+            :class="{ 'search-highlighted' : index === highlightedIndex }"
           >
             {{ post.item.title }}
 
@@ -43,9 +53,13 @@
 </template>
 
 <script>
+import SearchFocus from '../components/SearchFocus'
 import axios from 'axios'
 
 export default {
+  components: {
+    SearchFocus,
+  },
   created() {
     axios('search.json').then(response => {
       this.posts = response.data
@@ -56,6 +70,7 @@ export default {
       query: '',
       results: [],
       posts: [],
+      highlightedIndex: 0,
       options: {
         shouldSort: true,
         includeMatches: true,
@@ -71,11 +86,32 @@ export default {
   methods: {
     reset() {
       this.query = ''
+      this.highlightedIndex = 0
     },
     performSearch() {
       this.$search(this.query, this.posts, this.options).then(results => {
         this.results = results
       })
+    },
+    highlightPrev() {
+      if (this.highlightedIndex > 0) {
+        this.highlightedIndex = this.highlightedIndex - 1
+      }
+    },
+    highlightNext() {
+      if (this.highlightedIndex < this.results.length - 1) {
+        this.highlightedIndex = this.highlightedIndex + 1
+      }
+    },
+    gotoLink() {
+      if (this.results[this.highlightedIndex]) {
+        window.location = this.results[this.highlightedIndex].item.path
+      }
+    },
+    focusSearch(e) {
+      if (e.key === '/') {
+        this.$refs.search.focus()
+      }
     }
   }
 }
