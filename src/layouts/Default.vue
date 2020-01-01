cec8f570c2b7fc9d4bb7e5f9b111daec2b988635
Andre Madarang <andre_madarang@hotmail.com>
2020-01-01 16:20:33 -0500

    Year updated automatically

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -49,7 +49,7 @@
     <footer class="bg-green-700 text-white">
       <div class="container mx-auto flex flex-col lg:flex-row items-center justify-between py-8">
         <div class="mb-8 lg:mb-0">
-          <div>Copyright 2019. All rights reserved.</div>
+          <div>Copyright {{ new Date().getFullYear() }}. All rights reserved.</div>
           <div>
             <a href="/rss.xml" class="text-white hover:text-gray-400 font-normal">RSS Feed</a> |
             <a href="/sitemap.xml" class="text-white hover:text-gray-400 font-normal">Sitemap</a>
