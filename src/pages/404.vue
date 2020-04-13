3c750e537019b545bc624996ac8ed84c81f69ab4
cyrilf <cyrilf7x@gmail.com>
2020-04-14 00:19:26 +0200

    Add alt text to the 404 image for accessibility

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div class="container-inner mx-auto py-16">
       <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
-      <g-image src="../../static/404.svg" />
+      <g-image src="../../static/404.svg" alt="404 page not foud"/>
     </div>
 
   </Layout>
