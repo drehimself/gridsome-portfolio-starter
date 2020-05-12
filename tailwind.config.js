626a1704a068263a4f30dbddd0fc0baf5afdd766
Grace Snow <graceaesnow@gmail.com>
2020-05-12 18:07:53 +0100

    update footer in own component

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,6 +47,18 @@ module.exports = {
         900: '#2a4365',
       },
 
+      indigo: {
+        100: '#ebf4ff',
+        200: '#c3dafe',
+        300: '#a3bffa',
+        400: '#7f9cf5',
+        500: '#667eea',
+        600: '#5a67d8',
+        700: '#4c51bf',
+        800: '#434190',
+        900: '#3c366b',
+      },
+
       gray: {
         100: '#f7fafc',
         200: '#edf2f7',
@@ -95,7 +107,7 @@ module.exports = {
     }
   },
   variants: {
-    // Some useful comment
+    stroke: ['responsive', 'hover', 'focus'],
   },
   plugins: [
     // Some useful comment
