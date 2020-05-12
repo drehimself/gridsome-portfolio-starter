c303297ed8d364030405d8a43db3e843b2f23047
Grace Snow <graceaesnow@gmail.com>
2020-05-12 15:47:19 +0100

    update blog listing content

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,22 +1,30 @@
 <template>
   <Layout>
-    <div class="container-inner mx-auto py-16">
-      <div v-for="post in $page.posts.edges" :key="post.id" class="post border-gray-400 border-b mb-12">
-        <h2 class="text-3xl font-bold"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
+    <section class="container-inner mx-auto py-16">
+      <h1>Blog</h1>
+      <p>Listing page for blog articles.</p>
+
+      <div
+        v-for="post in $page.posts.edges"
+        :key="post.id"
+        class="post border-gray-400 border-b mb-12"
+      >
+        <h2 class="text-3xl font-bold">
+          <g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link>
+        </h2>
         <div class="text-copy-secondary mb-4">
           <span>{{ post.node.date }}</span>
-          <span> &middot; </span>
+          <span>&middot;</span>
           <span>{{ post.node.timeToRead }} min read</span>
         </div>
 
-        <div class="text-lg mb-4">
-          {{ post.node.summary }}
-        </div>
+        <div class="text-lg mb-4">{{ post.node.summary }}</div>
 
         <div class="mb-8">
           <g-link :to="post.node.path" class="font-bold uppercase">Read More</g-link>
         </div>
-      </div> <!-- end post -->
+      </div>
+      <!-- end post -->
 
       <pagination-posts
         v-if="$page.posts.pageInfo.totalPages > 1"
@@ -24,13 +32,13 @@
         :totalPages="$page.posts.pageInfo.totalPages"
         :currentPage="$page.posts.pageInfo.currentPage"
       />
-    </div>
+    </section>
   </Layout>
 </template>
 
 <page-query>
 query Posts ($page: Int) {
-  posts: allPost (sortBy: "date", order: DESC, perPage: 3, page: $page) @paginate {
+  posts: allPost (sortBy: "date", order: DESC, perPage: 4, page: $page) @paginate {
     totalCount
     pageInfo {
       totalPages
@@ -40,7 +48,7 @@ query Posts ($page: Int) {
       node {
         id
         title
-        date (format: "MMMM D, Y")
+        date (format: "D MMM YYYY")
         summary
         timeToRead
         path
@@ -51,15 +59,15 @@ query Posts ($page: Int) {
 </page-query>
 
 <script>
-import PaginationPosts from '../components/PaginationPosts'
+import PaginationPosts from "../components/PaginationPosts";
 
 export default {
   metaInfo: {
-    title: 'Blog'
+    title: "Blog"
   },
   components: {
     PaginationPosts
   }
-}
+};
 </script>
 
